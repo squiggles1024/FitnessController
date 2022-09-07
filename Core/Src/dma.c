0cbb31711add7ae5e88c7b31448969c47d3494bb
Evan Lemson <evanlemson@gmail.com>
2022-09-07 09:51:32 -0700

    Disable DMA Transfer Complete IRQ

--- Core/Src/dma.c
+++ Core/Src/dma.c
@@ -57,7 +57,7 @@ void ADC_DMA_Init(){
 	  /* DMA interrupt init */
 	  /* DMA1_Channel1_IRQn interrupt configuration */
 	  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
-	  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+	  //HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
 
 }
 /* USER CODE END 2 */
